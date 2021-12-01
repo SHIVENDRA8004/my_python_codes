48fcaf7c86527db70e77c6df7bacd1f3ded3911f
SHIVENDRA JAT <73995062+SHIVENDRA8004@users.noreply.github.com>
2021-12-02 01:27:49 +0530

    Delete ques2.py

--- ques2.py
+++
@@ -1,8 +0,0 @@
-def areaoftriangle(b,h):
-    print(f"The Base of triangle is: {b}")
-    print(f"The Height of triangle is: {h}")
-    area=(b*h)/2
-    print(f"Area of Triangle is: {area}")
-b=int(input("Enter The Base of triangle:"))
-h=int(input("Enter The Height of triangle:"))
-areaoftriangle(b,h)
