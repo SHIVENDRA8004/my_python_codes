189af703c475f42b08394831d9bb007b6ed21db5
SHIVENDRA JAT <73995062+SHIVENDRA8004@users.noreply.github.com>
2021-12-02 01:28:43 +0530

    Delete ques7.py

--- ques7.py
+++
@@ -1,7 +0,0 @@
-def areacircle(d):
-    print(f"The Diameter of circle is {d}")
-    pi=3.141
-    area=(pi*(d*d))/4
-    print(f"The Area of circle is {area}")
-d=int(input("Enter The Diameter Of Circle:"))
-areacircle(d)
