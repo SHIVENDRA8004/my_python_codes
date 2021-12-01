c73d2c5fd8ad11d4b3f0c700c65f1edbb4253719
SHIVENDRA JAT <73995062+SHIVENDRA8004@users.noreply.github.com>
2021-12-02 01:28:28 +0530

    Delete ques9.py

--- ques9.py
+++
@@ -1,9 +0,0 @@
-def volumnofcylinder(d,h):
-    print(f"The Diameter of Cylinder is {d}")
-    print(f"The Height of Cylinder is {h}")
-    pi=3.141
-    volumn=(pi*(d*d)*h)/4
-    print(f"The Volumn of Cylinder is {volumn}")
-d=int(input("Enter The Diameter Of Cylinder:"))
-h=int(input("Enter The Height Of Cylinder:"))
-volumnofcylinder(d,h)
