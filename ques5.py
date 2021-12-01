f988d18b915dab52e3b1edf9c5f91942e543cb4d
SHIVENDRA JAT <73995062+SHIVENDRA8004@users.noreply.github.com>
2021-12-02 01:28:13 +0530

    Delete ques5.py

--- ques5.py
+++
@@ -1,6 +0,0 @@
-def celsfehre(c):
-    print(f"The temperature in Celsius is : {c}")
-    f=((9*c)/5)+32
-    print(f"The temperature in Fehrenheit is : {f}")
-c=int(input("Enter The Temperature in Celsius: "))
-celsfehre(c)
