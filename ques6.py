c780b8e693b90b3675aaceec95440d0c5d9cb04b
SHIVENDRA JAT <73995062+SHIVENDRA8004@users.noreply.github.com>
2021-12-02 01:28:20 +0530

    Delete ques6.py

--- ques6.py
+++
@@ -1,6 +0,0 @@
-def fehrecels(f):
-    print(f"The temperature in Fehrenheit is : {f}")
-    c=(f-32)*(5/9)
-    print(f"The temperature in Celsius is : {c}")
-f=int(input("Enter The Temperature in Fehrenheit: "))
-fehrecels(f)
